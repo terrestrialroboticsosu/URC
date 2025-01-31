7a435c77d8d61b2ea4c0164001ffe5518fd088c4
redacted-riri <sariah.echols.18@gmail.com>
2025-01-31 10:45:19 -0500

    Basic Radio Documentation Added

---
+++ Documentation/Antenna.md
@@ -0,0 +1,10 @@
+# Antenna Documentation
+
+## Models
+https://learn.adafruit.com/feather-rp2040-rfm95/circuitpython 
+
+## Links 
+https://cdn-learn.adafruit.com/downloads/pdf/feather-rp2040-rfm95.pdf 
+
+## Setup
+Need to download mpy library while using python, get adafruit_rfm9x.mpy downloaded on the rp2040.
---
+++ Radio/send.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2023 Kattni Rembor for Adafruit Industries
+# SPDX-License-Identifier: MIT
+import board
+import digitalio
+import keypad
+import adafruit_rfm9x
+import time
+# Set up button using keypad module.
+button = keypad.Keys((board.BUTTON,), value_when_pressed=False)
+# Define radio frequency in MHz. Must match your
+# module. Can be a value like 915.0, 433.0, etc.
+RADIO_FREQ_MHZ = 915.0
+# Define Chip Select and Reset pins for the radio module.
+CS = digitalio.DigitalInOut(board.RFM_CS)
+RESET = digitalio.DigitalInOut(board.RFM_RST)
+# Initialise RFM95 radio
+rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
+while True:
+    print("sending")
+    time.sleep(0.5)
+    # This just sends  the info in a loop to avoid if statement issues
+    rfm95.send(bytes("hello world", "UTF-8"))
+    button_press = button.events.get()
+    if button_press:
+        if button_press.pressed:
+            rfm95.send(bytes("button", "UTF-8"))
